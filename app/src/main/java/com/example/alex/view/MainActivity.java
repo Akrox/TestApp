08abdbcfee1dd88fe79a1361440a1288822ed0b6
Alexander Avila <alex24.bcn@gmail.com>
2015-08-20 20:28:09 +0200

    Example button

--- app/src/main/java/com/example/alex/view/MainActivity.java
+++ app/src/main/java/com/example/alex/view/MainActivity.java
@@ -2,46 +2,71 @@ package com.example.alex.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.alex.testapp.R;
 
 public class MainActivity  extends Activity {
 
+    private TextView oka;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setListeners();
     }
+
+    private void findViews() {
+        oka = (TextView) findViewById(R.id.oka_text);
+    }
+
+    private void setListeners() {
+        oka.setOnClickListener(onClickOkaListener);
+        oka.setOnLongClickListener(onLongClickOkaListener);
+    }
+
+    View.OnClickListener onClickOkaListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            oka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_180));
+        }
+    };
+
+    View.OnLongClickListener onLongClickOkaListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            oka.setBackgroundColor(getResources().getColor(R.color.black));
+            return true;
+        }
+    };
 
     @Override protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
     }
 
     @Override protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
     }
 
     @Override protected void onPause() {
-        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
         super.onPause();
     }
 
     @Override protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
     }
 
     @Override protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
     }
 
     @Override protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 
 }
