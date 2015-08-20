4907b3100afc8dd1daea348250e97d958b2fa694
Akrox <alex24.bcn@gmail.com>
2015-08-20 18:34:33 +0200

    First Activity

--- app/src/main/java/androidTest/test/ApplicationTest.java
+++ app/src/main/java/androidTest/test/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.alex.testapp;
+package androidTest.test;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ app/src/main/java/com/example/alex/view/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.alex.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.alex.testapp.R;
+
+public class MainActivity  extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+}
